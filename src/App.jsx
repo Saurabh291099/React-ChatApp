2c72b2224ac81e05cf33026ca0718c59a92ecfe3
Saurabh291099 <110129942+Saurabh291099@users.noreply.github.com>
2024-06-14 16:00:51 +0530

    setup commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+
+      <p className="read-the-docs">
+        Hello world with chatApp
+      </p>
+    </>
+  )
+}
+
+export default App
